38a4eec48899ae363a166034dd5085736b4a2edb
aryzae <a.aryzae@gmail.com>
2019-05-22 23:05:09 +0900

    add ApplePayWrapper file

---
+++ ApplePaySample/ApplePaySample/ApplePayWrapper.swift
@@ -0,0 +1,14 @@
+//
+//  ApplePayWrapper.swift
+//  ApplePaySample
+//
+//  Created by Sho Ito on 2019/05/22.
+//  Copyright © 2019 aryzae. All rights reserved.
+//
+
+import Foundation
+import PassKit
+
+final class ApplePayWrapper {
+
+}
