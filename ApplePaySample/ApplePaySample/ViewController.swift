a3c346344ea2902bb5525b66dbb1d15f3e3a593b
aryzae <a.aryzae@gmail.com>
2019-05-22 23:03:08 +0900

    add project files

---
+++ ApplePaySample/ApplePaySample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ApplePaySample
+//
+//  Created by Sho Ito on 2019/05/22.
+//  Copyright © 2019 aryzae. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
