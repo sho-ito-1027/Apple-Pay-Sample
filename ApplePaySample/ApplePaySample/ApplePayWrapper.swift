2516f5dfaf70f8ce9c207b55d80c9bee14822787
aryzae <a.aryzae@gmail.com>
2019-05-22 23:34:58 +0900

    define static Apple Pay setting value

--- ApplePaySample/ApplePaySample/ApplePayWrapper.swift
+++ ApplePaySample/ApplePaySample/ApplePayWrapper.swift
@@ -9,6 +9,25 @@
 import Foundation
 import PassKit
 
+/// 静的な設定変数の集まり
+private struct ApplePaySetting {
+    /// 通貨
+    let currencyCode = "JPY"
+    /// 国コード
+    let countryCode = "JP"
+    /// AppleDeveloperで登録したID
+    let merchantIdentifier = ""
+    /// ApplePayの支払いの対象とするカードブランド
+    /// (iOSのversionによって指定できるものが異なる)
+    let paymentNetworks: [PKPaymentNetwork] = [.visa, .masterCard]
+    /// 請求先の情報。指定している情報は、ユーザがApplePay認証完了後のpaymentに含まれて入手できる
+    /// 指定した項目は、ユーザにとって必須入力となる。(実用上クレカが代理請求するので、指定しなくても困らない)
+    let billingAddressFields: [PKAddressField] = []
+    /// 配送先の情報。指定している情報は、ユーザがApplePay認証完了後のpaymentに含まれて入手できる
+    /// 指定した項目は、ユーザにとって必須入力となる。
+    let shippingAddressFields: [PKAddressField] = [.all]
+}
+
 final class ApplePayWrapper {
 
 }
